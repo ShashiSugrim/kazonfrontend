0d34769cc86b06f0d2f181660c315b22c042ad65
Shashi Sugrim <ssugrim1430@bths.edu>
2023-07-19 21:22:49 -0400

    added basic products grid

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Welcome To Kazon!</h2>
+    <>
+    <div>
+        <h2 style={{textAlign: "center"}}>Kazon</h2>
+        </div>
+      <div className="parent" >
+      
+        <div className="child">
         <div className="Search-bar">
           <input
             type="text"
@@ -18,8 +22,26 @@ function App() {
             placeholder="Search" // Add this line
           />
         </div>
-      </header>
-    </div>
+        </div>
+        <div className="child">
+        <button>Sign in</button>
+        </div>
+      </div>
+      <h3 style={{ backgroundColor: 'blue' }}>
+        <button> Customer Service</button>
+        <button>Best Sellers</button>
+      </h3>
+      <div>
+      <ul class="flex-container">
+  <li class="flex-item">1</li>
+  <li class="flex-item">2</li>
+  <li class="flex-item">3</li>
+  <li class="flex-item">4</li>
+  <li class="flex-item">5</li>
+  <li class="flex-item">6</li>
+</ul>
+      </div>
+    </>
   );
 }
 
