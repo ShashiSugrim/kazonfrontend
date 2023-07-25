1c719f0f75bd8df7bb231ffed8bdae66c8f77ca5
Shashi Sugrim <ssugrim1430@bths.edu>
2023-07-25 11:00:26 -0400

    added product page component

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Product from "./Product";
+import reactLogo from "../../src/logo192.png";
 
 const ProductPage = () => {
+  const navigate = useNavigate();
+  function handleClick(event) {
+    navigate("/about");
+  }
+
   return (
     <div>
-      <ul class="flex-container">
-  <li class="flex-item">1</li>
-  <li class="flex-item">2</li>
-  <li class="flex-item">3</li>
-  <li class="flex-item">4</li>
-  <li class="flex-item">5</li>
-  <li class="flex-item">6</li>
-</ul>
-      </div>
-  )
-}
+      <ul className="flex-container">
+        <Product source={reactLogo} title="PRoduct 1" price="$15"></Product>
+        <Product source={reactLogo} title="PRoduct 2" price="$20">
+          1
+        </Product>
+        <li className="flex-item">3</li>
+        <li className="flex-item">4</li>
+        <Product source={reactLogo} title="PRoduct 5" price="$50">
+          1
+        </Product>
+        <li className="flex-item">6</li>
+      </ul>
+    </div>
+  );
+};
 
-export default ProductPage
+export default ProductPage;
