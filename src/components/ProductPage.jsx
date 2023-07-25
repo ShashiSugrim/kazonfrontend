24a5b183f57b7b73d94407908b2fac0b4487735b
Shashi Sugrim <ssugrim1430@bths.edu>
2023-07-25 17:21:28 -0400

    remove unnecessary navigate and added css import

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,14 +1,9 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import Product from "./Product";
 import reactLogo from "../../src/logo192.png";
+import "../components/css/ProductPage.css";
 
 const ProductPage = () => {
-  const navigate = useNavigate();
-  function handleClick(event) {
-    navigate("/about");
-  }
-
   return (
     <div>
       <ul className="flex-container">
