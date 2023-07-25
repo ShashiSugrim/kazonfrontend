9e54a9933021f860d5087d08fdd584e2087181f0
Shashi Sugrim <ssugrim1430@bths.edu>
2023-07-25 17:18:05 -0400

    functional

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,34 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import TopBar from "../components/TopBar";
+import NavBar from "../components/NavBar";
+import ProductPage from "../components/ProductPage";
+import shoppingCart from "../kazonShoppingCart.jpeg"; // Adjust the path to match your project structure
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useNavigate,
+} from "react-router-dom";
+import SignIn from "../components/SignIn";
+import BestSellers from "./BestSellers";
+import LogoBar from "./LogoBar";
+import CustomerService from "./CustomerService";
 
 export default function Home() {
   return (
     <>
-      <h1>Home page</h1>
-      <Link to="/about">About</Link>
+      <LogoBar />
+      <TopBar />
+      <NavBar />
+      {/* <nav>
+          <Link to="/signin">Sign in</Link>
+        </nav> */}
+      <Routes>
+        <Route path="/" element={<ProductPage />} />
+        <Route path="bestsellers" element={<BestSellers />} />
+        <Route path="customerservice" element={<CustomerService />} />
+      </Routes>
     </>
   );
 }
