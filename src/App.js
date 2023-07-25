e9592244b5027b9d4ded9f36e4becb6bd6a0f79f
Shashi Sugrim <ssugrim1430@bths.edu>
2023-07-25 10:50:00 -0400

    refactored file into components and routing

--- src/App.js
+++ src/App.js
@@ -3,45 +3,45 @@ import "./App.css";
 import TopBar from "./components/TopBar";
 import NavBar from "./components/NavBar";
 import ProductPage from "./components/ProductPage";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useNavigate,
+} from "react-router-dom";
+import Home from "./components/Home";
+import SignIn from "./components/SignIn";
 
 function App() {
+  const navigate = useNavigate();
+
+  function handleLogo(event) {
+    navigate("/");
+  }
   return (
     <>
-    <div>
-        <h2 style={{textAlign: "center", color:'yellow',backgroundColor:'#3878f7'}}>Kazon </h2>
-        </div>
-      <div className="parent" >
-        <div className="child">
-        <div className="Search-bar">
-          <input
-            type="text"
-            id="name"
-            name="name"
-            required
-            minlength="7"
-            maxlength="50"
-            size="30"
-            placeholder="Search" // Add this line
-          />
-        </div>
-        </div>
-        <div className="child">
-        <button>Sign in</button> 
-        </div>
-      </div>
-      <h3 style={{ backgroundColor: '#3878f7' }}>
-        <button> Customer Service</button>
-        <button>Best Sellers</button>
-      </h3>
-      <div>
-      <ul class="flex-container">
-  <li class="flex-item">1</li>
-  <li class="flex-item">2</li>
-  <li class="flex-item">3</li>
-  <li class="flex-item">4</li>
-  <li class="flex-item">5</li>
-  <li class="flex-item">6</li>
-</ul>
+      <div className="App">
+        <h2
+          style={{
+            textAlign: "center",
+            color: "yellow",
+            backgroundColor: "#3878f7",
+          }}
+        >
+          Kazon{" "}
+        </h2>
+
+        <TopBar />
+        <NavBar />
+        {/* <nav>
+          <Link to="/signin">Sign in</Link>
+        </nav> */}
+        <Routes>
+          <Route path="/" element={<ProductPage />} />
+          <Route path="about" element={<Home />} />
+          <Route path="signin" element={<SignIn />} />
+        </Routes>
       </div>
     </>
   );
