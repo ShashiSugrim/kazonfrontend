4ec2ff04644683688219b5a6d1a8b0741ed579a5
Shashi Sugrim <ssugrim1430@bths.edu>
2023-07-25 11:00:39 -0400

    added top bar component

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,33 +1,53 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import shoppingCart from "../kazonShoppingCart.jpeg";
 
 const TopBar = () => {
+  const navigate = useNavigate();
+  function handleClick(event) {
+    console.log("Signed in");
+
+    navigate("/about");
+  }
+
   return (
     <>
-    <div>
-    <h2 style={{textAlign: "center"}}>Kazon</h2>
-    </div>
-  <div className="parent" >
-  
-    <div className="child">
-    <div className="Search-bar">
-      <input
-        type="text"
-        id="name"
-        name="name"
-        required
-        minlength="7"
-        maxlength="50"
-        size="30"
-        placeholder="Search" // Add this line
-      />
-    </div>
-    </div>
-    <div className="child">
-    <button>Sign in</button>
-    </div>
-  </div>
-  </>   
-  )
-}
+      <div className="parent">
+        <div className="child">
+          <div className="Search-bar">
+            <input
+              type="text"
+              id="name"
+              name="name"
+              required
+              minlength="7"
+              maxlength="50"
+              size="30"
+              placeholder="Search" // Add this line
+            />
+          </div>
+        </div>
+        <div className="child">
+          <button>
+            {" "}
+            <h3 style={{ textAlign: "right" }}>Sign in</h3>
+          </button>
+          {/*  
+         </div>
+         <div className="child">
+       <button> <img src = {shoppingCart} alt= 'shoppingCart'/></button> 
+       */}
+          <button className="shoppingCartButton">
+            <img
+              className="shoppingCartImage"
+              src={shoppingCart}
+              alt="shoppingCart"
+            />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default TopBar
+export default TopBar;
