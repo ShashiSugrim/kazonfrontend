b25dca65a260586893b22988abe319bfd667347b
Shashi Sugrim <ssugrim1430@bths.edu>
2023-07-26 11:33:04 -0400

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import App from "./App";
 
