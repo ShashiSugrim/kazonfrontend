09e50012c18f431c420f3f54006190b154a14aca
Shashi Sugrim <ssugrim1430@bths.edu>
2023-07-27 21:02:27 -0400

    using css modules

--- src/components/CustomerService.jsx
+++ src/components/CustomerService.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "../components/css/CustomerService.css";
+import CustomerServiceCSS from "../css/CustomerService.module.css";
 
 const CustomerService = () => {
   return (
     <>
-      <h2 className="cs">Welcome to Customer Service</h2>
-      <h3 className="cs">Call us at 1-800-888-KAZN</h3>
+      <h2 className={CustomerServiceCSS.cs}>Welcome to Customer Service</h2>
+      <h3 className={CustomerServiceCSS.cs}>Call us at 1-800-888-KAZN</h3>
     </>
   );
 };
