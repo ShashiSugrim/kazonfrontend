ded6d8dc434c599f1a3236784cc87d5805fc90b7
Shashi Sugrim <ssugrim1430@bths.edu>
2023-07-28 22:10:00 -0400

    component for search results

---
+++ src/components/SearchResults.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchResults = () => {
+  return (
+    <>
+      <h4 style={{ marginLeft: 20 }}>SearchResults for: </h4>
+    </>
+  );
+};
+
+export default SearchResults;
