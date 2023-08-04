bcc4ade1bba536a8c702abc99fa74e8ca9db17b4
Shashi Sugrim <ssugrim1430@bths.edu>
2023-08-04 17:05:10 -0400

    functions for get request for products

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Product from "./Product";
 import ProductPageCSS from "../css/ProductPage.module.css";
+import axios from "axios";
+import { useState } from "react";
 
 const ProductPage = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    // let ignore = false;
+
+    // if (!ignore)
+    getProducts();
+    // return () => {
+    //   ignore = true;
+    // };
+  }, []);
+  function getProducts() {
+    axios
+      .get("http://localhost:3001/products")
+      .then((response) => {
+        console.log("Responses are " + JSON.stringify(response.data));
+        setProducts(response.data);
+      })
+      .catch((err) => {
+        if (err) {
+          console.log("error" + err);
+        }
+      });
+  }
   return (
     <div>
       <ul className={ProductPageCSS.flexcontainer}>
