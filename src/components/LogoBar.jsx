f6b0dbddebc3c3843bef135279346b251822bc9d
Shashi Sugrim <ssugrim1430@bths.edu>
2023-08-14 18:25:49 -0400

    working sign out button

--- src/components/LogoBar.jsx
+++ src/components/LogoBar.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const LogoBar = () => {
@@ -6,17 +6,24 @@ const LogoBar = () => {
   const [username, setUsername] = useState([]);
 
   useEffect(() => {
-    const username = JSON.parse(localStorage.getItem('username'));
+    const username = JSON.parse(localStorage.getItem("username"));
     if (username) {
-     setUsername(username);
-    } else
-    {
+      setUsername(username);
+    } else {
       setUsername("Guest");
     }
   }, []);
   function goHome(event) {
     navigate("/");
   }
+
+  let handleSignOut = (event) => {
+    localStorage.setItem("accessToken", "null");
+    localStorage.setItem("username", JSON.stringify("Guest"));
+    alert("Signed out!");
+    window.location.reload(false);
+  };
+
   return (
     <div style={{ backgroundColor: "#3878f7", display: "flex" }}>
       <h2
@@ -31,10 +38,26 @@ const LogoBar = () => {
       </h2>
       <div
         style={{
-          color: "yellow",
-          marginRight: 40,
           textAlign: "right",
-          flexGrow: 1,
+          color: "yellow",
+          width: "20%",
+        }}
+      >
+        <button
+          onClick={handleSignOut}
+          style={{
+            marginTop: 10,
+            marginRight: 20,
+          }}
+        >
+          Sign out
+        </button>
+      </div>
+      <div
+        style={{
+          textAlign: "right",
+          color: "yellow",
+          width: "20%",
         }}
       >
         Signed in as: {username}
