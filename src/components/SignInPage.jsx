33531047a593e729b7da8d8dea2090782537ba37
Shashi Sugrim <ssugrim1430@bths.edu>
2023-08-14 14:38:44 -0400

    working sign in

--- src/components/SignInPage.jsx
+++ src/components/SignInPage.jsx
@@ -1,19 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 import SignInCSS from "../css/SignIn.module.css";
+import axios from "axios";
 
 const SignInPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignIn = async (event) =>
+  {
+    // localStorage.setItem('username', JSON.stringify('signedin'));
+    // console.log("username " + username);
+    await axios.post("http://localhost:3001/login", {email: username, password:password}).then((response)=>
+    {
+      if(response.data.accessToken === undefined)
+      {
+        localStorage.setItem('accessToken', 'null');
+        localStorage.setItem('username', JSON.stringify('Guest'));
+        }
+      else
+      {
+        localStorage.setItem('accessToken', response.data.accessToken);
+        localStorage.setItem('username', JSON.stringify(username));
+  
+      }
+      console.log("response is " + JSON.stringify(response.data))
+    }).catch((err) => {
+      if (err) {
+        console.log("error" + err);
+      }
+    });
+    console.log('localstorage auth is ' + localStorage.getItem('accessToken'));
+    window.location.reload(false);
+  }
+  
   return (
     <>
-      <form className={SignInCSS.form}>
-        <label for="fname">Username</label>
+      <form className={SignInCSS.form} onsubmit="return false">
+        <label for="fname">Email</label>
         <br />
-        <input type="text" id="fname" name="fname" />
+        <input type="text" value={username} onChange={(e)=>setUsername(e.target.value)} name="fname" />
         <br />
         <label for="lname">Password</label>
         <br />
-        <input type="text" id="lname" name="lname" />
+        <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)}  name="lname" />
         <br />
-        <button className={SignInCSS.signIn}>Sign In</button>
+        <button className={SignInCSS.signIn} type="button" onClick={handleSignIn}>Sign In</button>
       </form>
     </>
   );
