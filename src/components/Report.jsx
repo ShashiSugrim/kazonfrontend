c478c514e45dd9b316bc64ac1ebd6038bc62a73f
Shashi Sugrim <ssugrim1430@bths.edu>
2023-08-14 21:55:13 -0400

    working report function

--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import ReportCSS from "../css/Report.module.css";
+import axios from "axios";
 
 const Report = () => {
+  const [reportText, setReportText] = useState("");
+  const handleReport = async () =>
+  {
+    console.log("report text is " + reportText);
+    await axios.post("http://localhost:3001/report", {  text:reportText }, {headers: { "Authorization": localStorage.getItem('accessToken')} }).then((response) => {
+      alert(`Added report!`);
+    })
+    .catch((err) => {
+      // console.log(err.status)
+      if(err.response.status ===403)
+      {
+        alert("Max number of reports sent!");
+      }
+      if (err) {
+        console.log("error" + err);
+      }
+    });
+  }
   return (
     <>
       <h2 className={ReportCSS.head}>Enter your report below</h2>
       <form className={ReportCSS.form}>
-        <textarea name="reportText" className={ReportCSS.reportText} />
+        <textarea name="reportText" value={reportText} onChange={(e)=>{
+          setReportText(e.target.value);
+        }} className={ReportCSS.reportText} />
         <br></br>
-        <input type="submit" value="Submit" />
+        <button onClick={handleReport} type="button">Submit</button>
+
       </form>
+
     </>
   );
 };
